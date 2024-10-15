ef1da648f6258dd16786cb305c521b8297fbf083
Albin N <albin@jnilsson.id>
2024-10-15 14:05:44 +0200

    get data from preview url if present

--- src/Http/Controllers/PageController.php
+++ src/Http/Controllers/PageController.php
@@ -4,17 +4,24 @@ namespace Karabin\FabriqPlugin\Http\Controllers;
 
 use Ikoncept\Fabriq\Fabriq;
 use Ikoncept\Fabriq\Repositories\EloquentPageRepository;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Http;
 use Infab\Core\Traits\ApiControllerTrait;
 
 class PageController extends Controller
 {
     use ApiControllerTrait;
 
-    public function show(EloquentPageRepository $repo, Request $request, string $slug): JsonResponse
+    public function show(EloquentPageRepository $repo, Request $request, string $slug)
     {
+        if ($request->has('preview')) {
+            $url = base64_decode($request->preview);
+            $data = Http::get($url);
+
+            return $data->json();
+        }
+
         $result = $repo->findBySlug($slug);
 
         return $this->respondWithItem($result, Fabriq::getTransformerFor('live_page'));
