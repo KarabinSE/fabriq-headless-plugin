174584e38b0b4fff2f2b31c21ed68ffb6ac51996
Albin N <albin@jnilsson.id>
2026-09-15 11:25:44 +0200

    add semi-colon

--- src/Http/Controllers/ContactController.php
+++ src/Http/Controllers/ContactController.php
@@ -2,21 +2,20 @@
 
 namespace Karabin\FabriqPlugin\Http\Controllers;
 
-use Karabin\Fabriq\Fabriq;
 use Illuminate\Routing\Controller;
 use Karabin\Fabriq\Data\ContactData;
+use Karabin\Fabriq\Fabriq;
 use Spatie\LaravelData\DataCollection;
 
 class ContactController extends Controller
 {
-
     public function index()
     {
         $contacts = Fabriq::getFqnModel('contact')::where('published', 1)
             ->orderBy('sortindex')
             ->get();
 
-        return ContactData::collect($contacts, DataCollection::class)
+        return ContactData::collect($contacts, DataCollection::class);
     }
 
     public function show(int $id)
